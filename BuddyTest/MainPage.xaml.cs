cbc6e34fc60ac7fad1b3032d492d5d1e21dbcaec
Brad Serbus <brad_serbus@msn.com>
2012-09-22 11:53:50 -0700

    Remove sample code remains.

--- BuddyTest/MainPage.xaml.cs
+++ BuddyTest/MainPage.xaml.cs
@@ -16,17 +16,13 @@ namespace BuddyTest
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
 
-            // Set the data context of the listbox control to the sample data
-            DataContext = App.ViewModel;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
 
-        // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             var x = typeof(UserGender);
