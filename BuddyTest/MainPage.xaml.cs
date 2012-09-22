20b05eaf0d021ba459047fb7b4305a4be7420825
Brad Serbus <brad_serbus@msn.com>
2012-09-22 11:38:11 -0700

    User info entry, create Buddy user.

---
+++ BuddyTest/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Buddy;
+
+namespace BuddyTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var x = typeof(UserGender);
+
+            this.Gender.ItemsSource = this.GetNamesFromEnum(typeof(UserGender));
+
+            this.Status.ItemsSource = this.GetNamesFromEnum(typeof(UserStatus));
+        }
+
+        private IOrderedEnumerable<string> GetNamesFromEnum(Type enumType)
+        {
+            return enumType.GetFields().Where(fi => fi.IsLiteral).Select(fi => fi.Name).OrderBy(name => name);
+        }
+
+        private void Submit_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            this.CreateUser();
+        }
+
+        private void CreateUser()
+        {
+            BuddyClient client = new BuddyClient("brad_serbus@msn.com - Sample App", "81108202-A922-4D81-999A-2EFA7B554893");
+
+            /*client.LoginAsync((user, state) =>
+            {
+                // enable add user in first pivot
+            }, "username", "password");*/
+
+            var genderEnum = this.GetEnumValueFromListPickerItem<UserGender>(Gender);
+
+            var statusEnum = this.GetEnumValueFromListPickerItem<UserStatus>(Gender);
+
+            //client.CreateUserAsync((user, parameters) =>
+            //{
+            //}, Name.Text, Password.Text, genderEnum, int.Parse(Age.Text), Email.Text, statusEnum);       
+        }
+
+        private EnumType GetEnumValueFromListPickerItem<EnumType>(ListPicker listPicker)
+        {
+            return (EnumType)Enum.Parse(typeof(EnumType), (string)((ListPickerItem)Gender.SelectedItem).Content, false);
+        }
+    }
+}
---
+++ BuddyTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuddyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuddyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c27772c-9880-460f-9e60-f9109b4c3766")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
